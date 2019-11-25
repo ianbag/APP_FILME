f03cef34e2ab60c70a41b226af935d00df8781ff
Ian Bagliotti <ian.bagliotti@programmers.com.br>
2019-11-25 10:17:25 -0300

    consumindo service de delete de movies

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieDetail } from '../shared/movie-detail.model';
 import { MovieDetailService } from '../shared/movie-detail.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-movie-details',
@@ -12,16 +13,31 @@ export class MovieDetailsComponent implements OnInit {
   movies: MovieDetail[] = null
 
   constructor(
-    private movieDetailService: MovieDetailService
+    private movieDetailService: MovieDetailService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
     this.listMovies()
   }
 
-  listMovies(){
-    this.movieDetailService.get().subscribe(movies => 
+  listMovies() {
+    this.movieDetailService.get().subscribe(movies =>
       this.movies = movies
     )
   }
+
+  deleteMovie(id: number) {
+    this.movieDetailService.delete(id).subscribe(
+      res => {
+        this.toastr.success(`Filme ${res.nome} deletado!`, 'Sucesso');
+        this.listMovies();
+      },
+      error => {
+        this.toastr.error(`Houve um problema ao deletar.`, 'Erro');
+        console.warn('error ao deletar filme: ', error);
+      }
+    )
+  }
+
 }
