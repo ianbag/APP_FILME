20646a0a9e9fc14ebdbdb73d624d61bb7d3feed1
Ian Bagliotti <ian.bagliotti@programmers.com.br>
2019-11-26 11:47:55 -0300

    using takeUntil to unsubscribe of observable

--- src/app/movie-details/movie-detail/movie-detail.component.ts
+++ src/app/movie-details/movie-detail/movie-detail.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MovieDetailService } from 'src/app/shared/movie-detail.service';
 import { MovieDetail } from 'src/app/shared/movie-detail.model';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: []
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
 
   private id: number = null
   private movie: MovieDetail
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     private movieDetailService: MovieDetailService,
@@ -19,12 +22,19 @@ export class MovieDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.id = this.route.snapshot.params.id
+    this.id = this.route.snapshot.params.id;
     this.listMovie(this.id);
   }
 
-  listMovie(id: number){
-    this.movieDetailService.getById(id).subscribe(movie => this.movie = movie)
+  listMovie(id: number) {
+    this.movieDetailService.getById(id)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(movie => this.movie = movie);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
